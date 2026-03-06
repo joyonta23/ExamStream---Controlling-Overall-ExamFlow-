28974e020e6e3a3dc9959a7dd1a1b2fce2ead8d0
Joyonta Biswas <joyontabiswas2022@gmail.com>
2026-03-06 21:45:01 +0600

    Fix login crash when Google client ID is missing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,12 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const googleClientId =
+  process.env.REACT_APP_GOOGLE_CLIENT_ID || "disabled-google-oauth";
+
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={googleClientId}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
